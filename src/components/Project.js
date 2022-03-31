ba0b8c77841361b798cd3f7dbc719f734fa89db7
vohoang2005 <vohoang2005@gmail.com>
2022-03-30 23:57:30 -0700

    readd image

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,7 +12,7 @@ function Project(props) {
               <div className="card-image">
                 <figure className="image is-4by3">
                   <a href={project.live} target="_blank" rel="noreferrer">
-                    <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder" />
+                    <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
                   </a>
                 </figure>
               </div>
