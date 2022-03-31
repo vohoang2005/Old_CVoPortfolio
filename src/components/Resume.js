e64dc8cac94d7c55e1e38e77c862abd6919114bc
vohoang2005 <vohoang2005@gmail.com>
2022-03-30 21:45:28 -0700

    updated About Me

---
+++ src/components/Resume.js
@@ -0,0 +1,38 @@
+// Link to a downloadable resume
+// a list of the developer’s proficiencies
+
+import React from "react";
+
+function Resume() {
+  return (
+    <div className="columns">
+      <div className="column">
+        <p className="content is-medium">Resume</p>
+        <hr />
+
+        <a
+          className="button is-primary"
+          href={process.env.PUBLIC_URL + "/ChuongVoResume.pdf"}
+          target="_blank" rel="noreferrer"
+        >
+          <span className="icon">
+            <i className="fas fa-download"></i>
+          </span>
+          <span>Download My Resume</span>
+        </a>
+      </div>
+      <div className="column">
+        <p className="content is-medium">Skills</p>
+        <hr />
+        <ul>
+            <li>HTML, CSS, Javascript</li>
+            <li>API, NPM, JSON</li>
+            <li>Node, Bootstrap, Express</li>
+            <li>Git, MongoDB, Insomnia</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
