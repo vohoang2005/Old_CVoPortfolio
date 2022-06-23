ebf84e59edf6ac021b2be715ef5d8ef91887a2a3
Chuong Vo <vohoang2005@gmail.com>
2022-06-23 10:59:22 -0700

    centered Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
-
 import React from "react";
 
 function Footer() {
-    return <footer><p> copyright 2022</p></footer>
-}
+    return (
+      <footer className="footer">
+        <div class="text-center">
+          <p>
+            <strong>React Portfolio</strong> - by{" "}
+            <a href="https://github.com/vohoang2005" target="_blank" rel="noopener noreferrer">
+              Chuong Hoang Vo
+            </a>
+            .
+            <hr />
+
+            <a href="https://www.linkedin.com/in/chuonghvo/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>{" "}|{" "}
+            <a href="https://github.com/vohoang2005" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
 
 export default Footer;
