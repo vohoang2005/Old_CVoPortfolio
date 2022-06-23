1665f0fadad9d9e11861b41037132118b4d03136
Chuong Vo <vohoang2005@gmail.com>
2022-06-23 16:35:28 -0700

    installed tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-
-root.render(<App />);
+ReactDOM.render (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
