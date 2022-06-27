7220caa7ddc3b62586eb72e42099add6a8e0c085
Chuong Vo <vohoang2005@gmail.com>
2022-06-27 16:23:10 -0700

    updated Contact and Work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import Logo from '../assets/logo.png';
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -15,11 +16,31 @@ const Navbar = () => {
         </div>
         <div>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+              <Link to="home" smooth={true} offset={50} duration={500}>
+              Home
+            </Link>
+            </li>
+            <li>    
+              <Link to="about" smooth={true} offset={50} duration={500}>
+              About
+            </Link>
+            </li>
+            <li>              
+              <Link to="skills" smooth={true} offset={50} duration={500}>
+              Skills
+            </Link>
+            </li>
+            <li>              
+              <Link to="work" smooth={true} offset={50} duration={500}>
+              Works
+            </Link>
+            </li>
+            <li>              
+              <Link to="contact" smooth={true} offset={50} duration={500}>
+              Contact
+            </Link>
+            </li>
           </ul>
         </div>
 
@@ -29,12 +50,30 @@ const Navbar = () => {
           </div>
 
           {/* Mobile menu */}
-          <ul className={!nav ? 'hidden' : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+          <ul className={!nav 
+            ? 'hidden' 
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
+
+            <li className='py-6 text-4xl'>              
+            <Link onClick={handleClick} to="home" smooth={true} offset={50} duration={500}>
+              Home
+            </Link></li>
+            <li className='py-6 text-4xl'>              
+            <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500}>
+              About
+            </Link></li>
+            <li className='py-6 text-4xl'>              
+            <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500}>
+              Skills
+            </Link></li>
+            <li className='py-6 text-4xl'>              
+            <Link onClick={handleClick} to="work" smooth={true} offset={50} duration={500}>
+              Works
+            </Link></li>
+            <li className='py-6 text-4xl'>              
+            <Link onClick={handleClick} to="contact" smooth={true} offset={50} duration={500}>
+              Contact
+            </Link></li>
           </ul>
 
             {/* Social Icons */}
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,192 @@
 import React from 'react'
+import WorkImg from '../assets/css.png'
+import Blog from '../assets/Blog.png'
+import BudgetTracker from '../assets/BudgetTracker.png'
+import DrumKit from '../assets/DrumKit.png'
+import Maintainable from '../assets/Maintainable.png'
+import PWGenerator from '../assets/PWGenerator.png'
+import Quiz from '../assets/Quiz.png'
+import Randm from '../assets/randm.png'
+import SimonGame from '../assets/SimonGame.png'
+import ToDoList from '../assets/ToDoList.png'
 
 const Work = () => {
   return (
-    <div>
-        
+    <div name='work' className=' w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+                <p className='py-6'>Check out some of my recent work</p>
+            </div>
+
+{/* Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/* Grid Item */}
+                <div 
+                style={{backgroundImage: `url(${Blog})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Blog Website Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${BudgetTracker})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Budget Tracker Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${DrumKit})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            DrumKit Game
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${Maintainable})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Managing AC Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${PWGenerator})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Password Generator Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${Quiz})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Quiz Game
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${Randm})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Randm Dating Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${SimonGame})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Simon Game
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div 
+                style={{backgroundImage: `url(${ToDoList})`}} 
+                className='shadow-lg shadow-]#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            To Do List Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
